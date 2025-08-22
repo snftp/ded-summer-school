6c5f5efce5b1073f07c7ff956b10eab43bcefcf3
snftp <ale.drow@yandex.ru>
2025-08-23 00:04:29 +0300

    Tried to solve with one array which may contain all roots/nans/-1 but do not know the theory to implement it. Problem with passing an aaray to a function

--- SolveEquation.c
+++ SolveEquation.c
@@ -3,63 +3,67 @@
 #include <math.h>
 
 // TODO: discriminant, cntr parameter is unnecessary DONE
-int Counting_Solutions(double a, double b, double c,
-                        double *p0, double *p1, double *p2) { // NOTE: is it a good idea to have separate p0 for 1 root case? // TODO: maybe a better name for p0, p1, etc? Possibly use an array, double roots[2]?
+int Solutions(double a, double b, double c) { // NOTE: is it a good idea to have separate p0 for 1 root case? NO IF REPLACING ROOTS WITH ARRAY // TODO: maybe a better name for p0, p1, etc ROOTS? Possibly use an array, double roots[2] YES?
     // TODO: maybe this at the start, *p0 = *p1 = *p2 = NAN;
-    int cntr = 0
+    double roots[2] = {NAN, NAN}
     if (fabs(a) <= DBL_EPSILON) { // TODO: beware of the indentation DONE
             if (fabs(b) > DBL_EPSILON) {
-                cntr = 1;
-                *p0 = -1 * c / b;
-                *p1 = 0.0 / 0.0; // TODO: it's better to write NAN explicitly in such cases (= NAN)
-                *p2 = 0.0 / 0.0;
+                roots[0] = -1 * c / b;
+                // cntr = 1;
+                // *p0 = -1 * c / b;
+                // *p1 = 0.0 / 0.0; // TODO: it's better to write NAN explicitly in such cases (= NAN)
+                // *p2 = 0.0 / 0.0;
             }
             else {
                 if (fabs(c) <= DBL_EPSILON) {
-                    cntr = -1; // TODO: make a const that will explicitly say what this means, e.g. const int INFINITE_ROOTS = -1;
+                    roots[0] = -1 // INFINITY NUMBER OF ROOTS
+                    roots[1] = -1
+                    // cntr = -1; // TODO: make a const that will explicitly say what this means, e.g. const int INFINITE_ROOTS = -1;
                 }
-                else {
-                    cntr = 0;
-                }
+                // else {
+                //     значения в ROOTS останутся NAN поэтому не требуется прописывать ELSE
+                //     cntr = 0;
+                // }
 
                 // TODO: it's better to set roots here to NAN explicitly
             }
     }
     else {
         discriminant = b * b - 4 * a * c;
-        if (discriminant < 0) {
-            cntr = 0; // TODO:
-        }
-        else if (discriminant > 0) {
-            cntr = 2;
+        // if (discriminant < 0) { значения в ROOTS останутся NAN поэтому не требуется прописывать этот случай
+        //     cntr = 0;
+        // }
+        if (discriminant > 0) {
             sqrt_discriminant = sqrt(b * b - 4 * a * c)
-            *p0 = 0.0 / 0.0;
-            *p1 = (-1 * b + sqrt_discriminant) / (2 * a); // TODO: extract sqrt(discriminant), also you could make a variable sqrt_discriminant DONE
-            *p2 = (-1 * b - sqrt_discriminant) / (2 * a);
+            roots[0] = (-1 * b + sqrt_discriminant) / (2 * a); // TODO: extract sqrt(discriminant), also you could make a variable sqrt_discriminant DONE
+            roots[1] = (-1 * b - sqrt_discriminant) / (2 * a);
         }
         else {
-            cntr = 1;
-            *p0 = (-1 * b) / (2 * a);
-            *p1 = 0.0 / 0.0;
-            *p2 = 0.0 / 0.0;
+            if (fabs(discriminant) <= DBL_EPSILON) {
+                roots[0] = (-1 * b) / (2 * a);
+            // cntr = 1;
+            // *p0 = (-1 * b) / (2 * a);
+            // *p1 = 0.0 / 0.0;
+            // *p2 = 0.0 / 0.0;
+            }
         }
     }
-    return cntr;
+    return roots;
 }
 
 
-void Solutions(int cntr, double x0, double x1, double x2) {
-    if (cntr == 0) {
-        printf("NO ROOTS"); // TODO: add a newline? puts?
+void Solutions_Presentation(double roots[2]) {
+    if (roots[0] == -1) {
+        printf("INFINITE NUMBER OF ROOTS\n");
     }
-    else if (cntr == -1) {
-        printf("INFINITE NUMBER OF ROOTS");
+    else if (roots[0] == NAN) && (roots[1] == NAN) {
+        printf("NO ROOTS\n"); // TODO: add a newline? puts? DONE
     }
     else {
-        double arr[3] = {x0, x1, x2}; // NOTE: maybe you could store roots in an array to begin with?
-        for (int i = 0; i < 3; i++) {
-            if (!isnan(arr[i])) {
-                printf("ROOT(S): %lf", arr[i]);
+        // double arr[3] = {x0, x1, x2}; // NOTE: maybe you could store roots in an array to begin with? DONE
+        for (int i = 0; i < 2; i++) {
+            if (!isnan(roots[i])) {
+                printf("ROOT(S): %lf/n", roots[i]);
             }
         }
     }
@@ -67,11 +71,10 @@ void Solutions(int cntr, double x0, double x1, double x2) {
 
 // TODO: it's better to use full names when possible, prefer count over cntr
 int main() {
-    double a = 0.0, b = 0.0, c = 0.0, x0 = 0.0, x1 = 0.0, x2 = 0.0;
-    // TODO: maybe use an array? double roots[3] = { NAN, NAN, NAN };
-    int cntr_of_solutions = 0;
+    double a = 0.0, b = 0.0, c = 0.0;
     scanf("%lf %lf %lf", &a, &b, &c); // TODO: scanf can fail, check
-    cntr_of_solutions = Counting_Solutions(a, b, c, &x0, &x1, &x2);
-    Solutions(cntr_of_solutions, x0, x1, x2);
+    // TODO: maybe use an array? double roots[3] = { NAN, NAN, NAN }; DONE WITH 2 ROOTS
+    double roots[2] = Solutions(a, b, c);
+    Solutions_Presentation(roots);
     return 0;
 }
